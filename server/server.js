c10b5c1e5ae46b009076e108165076d88cb103b5
Fabs2308 <fabse231@googlemail.com>
2018-02-07 22:18:35 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -41,7 +43,17 @@ app.get('/todos', (req, res) => {
    });
 });
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id.toString();
+    if(!ObjectID.isValid(id)) return res.status(404).send();
 
+    Todo.findById(id).then((todo) => {
+        if(!todo)    return res.status(404).send();
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
